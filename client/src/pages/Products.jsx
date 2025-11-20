4b796dba0e4ef2820097f05f1f9a59edbeb334ae
moinharis <146499851+harisUOL@users.noreply.github.com>
2025-11-20 07:58:26 +0000

    edited files

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -18,7 +18,7 @@ const [size, setSize] = useState("");
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
     setLoading(true);
-    fetch("http://3.89.186.55/api/products")
+    fetch("/api/products")
       .then(res => res.json())
       .then(data => {
         setProducts(data);
